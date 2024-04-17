aa69f7709dbdf2e0fc194afaa97f4cbdc96fdd0d
StefDev1801 <stefano200118@gmail.com>
2024-04-17 16:33:33 +0200

    add development stack

---
+++ assets/app.js
@@ -0,0 +1,12 @@
+const { createApp } = Vue
+createApp({
+    data() {
+        return {}
+    },
+    mounted() {
+        console.log('test');
+    },
+    methods: {
+
+    }
+}).mount('#app')
--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+
+$string = file_get_contents('./assets/dischi.json');
+
+$dischi = json_decode($string, true);
+
+var_dump($dischi);
+
 ?>
 
 <!DOCTYPE html>
@@ -6,9 +13,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/style.css">
     <title>Document</title>
 </head>
 <body>
-    
+    <div>Test</div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.8/axios.min.js" integrity="sha512-PJa3oQSLWRB7wHZ7GQ/g+qyv6r4mbuhmiDb8BjSFZ8NZ2a42oTtAq5n0ucWAwcQDlikAtkub+tPVCw4np27WCg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="./assets/app.js"></script>
 </body>
 </html>
